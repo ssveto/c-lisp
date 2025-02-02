6802537e3265815346be411e365bbdb08f5f5ba3
ssveto <96317816+ssveto@users.noreply.github.com>
2025-02-02 12:08:04 +0100

    Initial commit

---
+++ first_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void helloWorld(int n) {
+    for (int i = 0; i < n; i++) {
+        puts("Hello sveto!");
+    }
+}
+
+int main() {
+
+    helloWorld(25);
+
+    return 0;
+}
